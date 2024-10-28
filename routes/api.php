df01d79a4dd69c3f916506d9dce569b7617a0eab
Y3sith <145672787+Y3sith@users.noreply.github.com>
2024-10-28 16:22:28 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([
+    'prefix' => 'auth'
+],function () {
+
+});
+
+Route::group([
+    'prefix' => 'product',
+    'middleware' => 'auth:api'
+],function () {
+    
+});
